6ee086378a9dc0712c6b95d211aa4409a3921305
Kyle Cribbs <kyle@clearscienceinc.com>
2020-06-12 09:53:59 -0400

    updating wording on success message to indicate double opt-in

--- js/mailing-list.js
+++ js/mailing-list.js
@@ -1,39 +1,36 @@
 function updateMailList() {
-    const Http = new XMLHttpRequest();
-    const data = {
-      email: document.getElementById('Email1').value,
-      name: document.getElementById('Name1').value,
-      description: "visitor",
-      subscribed: true,
+  const Http = new XMLHttpRequest();
+  const data = {
+    email: document.getElementById('Email1').value,
+    name: document.getElementById('Name1').value,
+    description: "visitor"
+  }
+  var url = `https://localhost:5001/account/AddListMember?email=${data.email}&name=${data.name}&description=${data.description}`;
+  Http.open("GET", url);
+  Http.send();
+  Http.onreadystatechange=function() {
+    var response = JSON.parse(Http.responseText);
+    var currentEmail = data.email;
+    if(response.message == "Mailing list member has been created")
+    {
+      $('#Success-tag').text("A verification email has been sent to your email address!");
+      setTimeout(function() {
+        $('#Success-tag').text("");
+      }, 5000);
+      document.getElementById('Email1').value = "";
+      document.getElementById('Name1').value = "";
     }
-
-    var url = `https://id.clearscienceinc.com/account/AddListMember?email=${data.email}&subscribed=${data.subscribed}&name=${data.name}&description=${data.description}`;
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange=function() {
-      var response = JSON.parse(Http.responseText);    
-      var currentEmail = data.email;
-      if(response.message == "Mailing list member has been created")
-      {
-        $('#Success-tag').text("Successfully Subscribed!");
-        setTimeout(function() {
-          $('#Success-tag').text("");
-        }, 5000);
-        document.getElementById('Email1').value = "";
-        document.getElementById('Name1').value = "";
-      }
-      else if(response.message == "The 'address' parameter should be a valid email address")
-      {
-        $('#Success-tag').text("Invalid Email!");
-        document.getElementById('Email1').value = "";
-        document.getElementById('Name1').value = "";
-      }
-      else if(response.message == "Address already exists '" + currentEmail.toLowerCase() + "'")
-      {
-        $('#Success-tag').text("Already Subscribed!");
-        document.getElementById('Email1').value = "";
-        document.getElementById('Name1').value = "";
-      }
+    else if(response.message == "The 'address' parameter should be a valid email address")
+    {
+      $('#Success-tag').text("Invalid Email!");
+      document.getElementById('Email1').value = "";
+      document.getElementById('Name1').value = "";
+    }
+    else if(response.message == "Address already exists '" + currentEmail.toLowerCase() + "'")
+    {
+      $('#Success-tag').text("Already Subscribed!");
+      document.getElementById('Email1').value = "";
+      document.getElementById('Name1').value = "";
     }
   }
- 
+}
